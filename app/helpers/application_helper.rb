be51a42e03bf40a61f1e928b8919391744804e23
Kristina Kostyuchenko <kostyuchenko.ks@gmail.com>
2017-06-01 22:41:36 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Возвращает полный заголовок для страницы.
+  def full_title(page_title)
+    base_title = "Tammantimebel"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
