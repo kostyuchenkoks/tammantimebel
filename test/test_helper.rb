7c32efce0807195c06b29f919622060773dc415b
Kristina Kostyuchenko <kostyuchenko.ks@gmail.com>
2017-06-03 04:02:55 +0700

    Depot Scaffold

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,24 +8,24 @@ class ActiveSupport::TestCase
 
   # Возвращает true, если тестовый пользователь вошел.
   def is_logged_in?
-	!session[:user_id].nil?
+    !session[:user_id].nil?
   end
 
   # Выполняет вход тестового пользователя.
   def log_in_as(user, options = {})
-	password = options[:password] || 'password'
-	remember_me = options[:remember_me] || '1'
-	if integration_test?
-	  post login_path, session: { email: user.email,
-								password: password,
-								remember_me: remember_me }
-	else
-	  session[:user_id] = user.id
-	end
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
   end
   private
-  # Возвращает true внутри интеграционного теста.
-  def integration_test?
-	defined?(post_via_redirect)
-  end
+    # Возвращает true внутри интеграционного теста.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
