20aa0296b406f1db936d84e67b1c7759a4a5104f
Kristina Kostyuchenko <kostyuchenko.ks@gmail.com>
2017-06-02 14:56:07 +0700

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,33 +9,34 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-  # Возвращает дайджест для указанной строки.
-  def User.digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ?
-	BCrypt::Engine::MIN_COST :
-	BCrypt::Engine.cost
-	BCrypt::Password.create(string, cost: cost)
+  class << self
+  # Возвращает хэш - дайджест для указанной строки.
+  def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ?
+      BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
   # Возвращает случайный токен.
-  def User.new_token
-	SecureRandom.urlsafe_base64
+  def new_token
+    SecureRandom.urlsafe_base64
   end
-
+end
   # Запоминает пользователя в базе данных для использования в постоянных сеансах.
   def remember
-	self.remember_token = User.new_token
-	update_attribute(:remember_digest, User.digest(remember_token))
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # Возвращает true, если указанный токен соответствует дайджесту.
   def authenticated?(remember_token)
-	return false if remember_digest.nil?
-	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   # Забывает пользователя
   def forget
-	update_attribute(:remember_digest, nil)
+    update_attribute(:remember_digest, nil)
   end
 end
