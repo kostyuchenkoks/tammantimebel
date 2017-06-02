b9a5df32099a36c2eb532b8c5ad2bc0c3e2fcf01
Kristina Kostyuchenko <kostyuchenko.ks@gmail.com>
2017-06-02 21:45:48 +0700

    Add account activations

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,23 @@
+class UserMailer < ApplicationMailer
+
+  # Тема письма может быть указана в файле I18n config/locales/en.yml
+  # следующим образом:
+  #
+  #   en.user_mailer.account_activation.subject
+  #
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
+  # Тема письма может быть указана в файле I18n config/locales/en.yml
+  # следующим образом:
+  #
+  #   en.user_mailer.password_reset.subject
+  #
+  def password_reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
