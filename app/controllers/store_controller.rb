b36a02d32a1d48bf2df312d429a55d32102e2091
Kristina Kostyuchenko <kostyuchenko.ks@gmail.com>
2017-06-05 03:57:16 +0700

    Update page for each item

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,9 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
+    #@products = Product.order(:title)
+    @products = Product.all
   end
+
+
 end
